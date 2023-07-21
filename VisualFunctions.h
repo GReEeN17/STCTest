0ad9bdcff1324dd016fea18fe3facf45fd12d06f
Aleksandr <azelen04@mail.ru>
2023-07-21 13:07:40 +0300

    Deleted not needed functions

--- VisualFunctions.h
+++ VisualFunctions.h
@@ -7,20 +7,6 @@
 #include "stb_easy_font.h"
 using std::cin, std::string, std::cout;
 
-void print_string(float x, float y, char *text, float r, float g, float b)
-{
-    static char buffer[99999]; // ~500 chars
-    int num_quads;
-
-    num_quads = stb_easy_font_print(x, y, text, NULL, buffer, sizeof(buffer));
-
-    glColor3f(r,g,b);
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glVertexPointer(2, GL_FLOAT, 16, buffer);
-    glDrawArrays(GL_QUADS, 0, num_quads*4);
-    glDisableClientState(GL_VERTEX_ARRAY);
-}
-
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
     glMatrixMode(GL_PROJECTION);
